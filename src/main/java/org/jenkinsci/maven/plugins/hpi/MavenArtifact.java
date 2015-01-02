jenkinsci/maven-hpi-plugin

commit ae5fdf6550d8d69b8325564e062fd973c54492fc

Provide a conversion code to related variants of MavenArtifact.
Use that to simplify BundlePluginMojo.

src/main/java/org/jenkinsci/maven/plugins/hpi/MavenArtifact.java
@@ -1,7 +1,12 @@
 package org.jenkinsci.maven.plugins.hpi;
 
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.AbstractArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.artifact.versioning.OverConstrainedVersionException;
 import org.apache.maven.project.MavenProject;
@@ -13,6 +18,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import static org.apache.maven.artifact.Artifact.SCOPE_COMPILE;
+
 /**
  * {@link Artifact} is a bare data structure without any behavior and therefore
  * hard to write OO programs around it.
@@ -22,13 +29,17 @@ import java.util.List;
  * @author Kohsuke Kawaguchi
  */
 public class MavenArtifact {
+    public final ArtifactFactory artifactFactory;
     public final MavenProjectBuilder builder;
     public final List<ArtifactRepository> remoteRepositories;
     public final ArtifactRepository localRepository;
     public final Artifact artifact;
+    public final ArtifactResolver resolver;
 
-    public MavenArtifact(Artifact artifact, MavenProjectBuilder builder, List<ArtifactRepository> remoteRepositories, ArtifactRepository localRepository) {
+    public MavenArtifact(Artifact artifact, ArtifactResolver resolver, ArtifactFactory artifactFactory, MavenProjectBuilder builder, List<ArtifactRepository> remoteRepositories, ArtifactRepository localRepository) {
         this.artifact = artifact;
+        this.resolver = resolver;
+        this.artifactFactory = artifactFactory;
         this.builder = builder;
         this.remoteRepositories = remoteRepositories;
         this.localRepository = localRepository;
@@ -79,9 +90,26 @@ public class MavenArtifact {
         return artifact.getType();
     }
 
+    /**
+     * Resolves to the jar file that contains the code of the plugin.
+     */
     public File getFile() {
-        // TODO: should we resolve?
+        if (artifact.getFile()==null)
+            try {
+                resolver.resolve(artifact, remoteRepositories, localRepository);
+            } catch (AbstractArtifactResolutionException e) {
+                throw new RuntimeException("Failed to resolve "+getId(),e);
+            }
         return artifact.getFile();
+    }
+
+    /**
+     * Returns {@link MavenArtifact} for the hpi variant of this artifact.
+     */
+    public MavenArtifact getHpi() {
+        Artifact a = artifactFactory
+                .createArtifact(artifact.getGroupId(), artifact.getArtifactId(), artifact.getVersion(), SCOPE_COMPILE, "hpi");
+        return new MavenArtifact(a,resolver,artifactFactory,builder,remoteRepositories,localRepository);
     }
 
     public List<String/* of IDs*/> getDependencyTrail() {

src/main/java/org/jenkinsci/maven/plugins/hpi/RunMojo.java
@@ -644,7 +644,7 @@ public class RunMojo extends AbstractJetty6Mojo {
     }
 
     protected MavenArtifact wrap(Artifact a) {
-        return new MavenArtifact(a,projectBuilder,getProject().getRemoteArtifactRepositories(),localRepository);
+        return new MavenArtifact(a,artifactResolver,artifactFactory,projectBuilder,getProject().getRemoteArtifactRepositories(),localRepository);
     }
 
     protected Artifact getJenkinsWarArtifact() throws MojoExecutionException {

src/main/java/org/jenkinsci/maven/plugins/hpi/AbstractHpiMojo.java
@@ -457,10 +457,6 @@ public abstract class AbstractHpiMojo extends AbstractJenkinsMojo {
         return r;
     }
 
-    protected MavenArtifact wrap(Artifact a) {
-        return new MavenArtifact(a,projectBuilder,project.getRemoteArtifactRepositories(),localRepository);
-    }
-
     /**
      * Builds the webapp for the specified project.
      * <p/>

src/main/java/org/jenkinsci/maven/plugins/hpi/AbstractJenkinsMojo.java
@@ -4,6 +4,7 @@ import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
 import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -56,6 +57,9 @@ public abstract class AbstractJenkinsMojo extends AbstractMojo {
     protected ArtifactFactory artifactFactory;
 
     @Component
+    protected ArtifactResolver artifactResolver;
+
+    @Component
     protected MavenProjectBuilder projectBuilder;
 
     @Component
@@ -78,6 +82,6 @@ public abstract class AbstractJenkinsMojo extends AbstractMojo {
     }
 
     protected MavenArtifact wrap(Artifact a) {
-        return new MavenArtifact(a,projectBuilder,remoteRepos,localRepository);
+        return new MavenArtifact(a,artifactResolver,artifactFactory,projectBuilder,remoteRepos,localRepository);
     }
 }

src/main/java/org/jenkinsci/maven/plugins/hpi/BundlePluginsMojo.java
@@ -68,7 +68,7 @@ public class BundlePluginsMojo extends AbstractJenkinsMojo {
             // This would be unnecessary and trivial if the hpi packaging was defined correctly
             ArtifactResolutionResult r = resolver.resolveTransitively(artifacts, project.getArtifact(), remoteRepos, localRepository, artifactMetadataSource);
 
-            List<Artifact> hpis = new ArrayList<Artifact>();
+            List<MavenArtifact> hpis = new ArrayList<MavenArtifact>();
             int artifactIdLength = 0; // how many chars does it take to print artifactId?
             int versionLength = 0;
 
@@ -96,11 +96,9 @@ public class BundlePluginsMojo extends AbstractJenkinsMojo {
             outputDirectory.mkdirs();
 
             for (MavenArtifact a : selected.values()) {
-                getLog().debug("Copying " + a.getFile());
+                MavenArtifact hpi = a.getHpi();
+                getLog().debug("Copying " + hpi.getFile());
 
-                Artifact hpi = artifactFactory
-                        .createArtifact(a.getGroupId(), a.getArtifactId(), a.getVersion(), SCOPE_COMPILE, "hpi");
-                resolver.resolve(hpi, remoteRepos, localRepository);
 
                 FileUtils.copyFile(hpi.getFile(), new File(outputDirectory, hpi.getArtifactId() + ".hpi"));
                 hpis.add(hpi);
@@ -108,12 +106,12 @@ public class BundlePluginsMojo extends AbstractJenkinsMojo {
                 versionLength = Math.max(versionLength, hpi.getVersion().length());
             }
 
-            Collections.sort(hpis, new Comparator<Artifact>() {
-                public int compare(Artifact o1, Artifact o2) {
+            Collections.sort(hpis, new Comparator<MavenArtifact>() {
+                public int compare(MavenArtifact o1, MavenArtifact o2) {
                     return map(o1).compareTo(map(o2));
                 }
 
-                private String map(Artifact a) {
+                private String map(MavenArtifact a) {
                     return a.getArtifactId();
                 }
             });
@@ -122,7 +120,7 @@ public class BundlePluginsMojo extends AbstractJenkinsMojo {
             list.getParentFile().mkdirs();
             PrintWriter w = new PrintWriter(list);
             try {
-                for (Artifact hpi : hpis) {
+                for (MavenArtifact hpi : hpis) {
                     getLog().info(String.format("%" + (-artifactIdLength) + "s    %" + (-versionLength) + "s    %s",
                             hpi.getArtifactId(), hpi.getVersion(), hpi.getGroupId()));
                     w.println(hpi.getId());

commit ee161c719bbc9dfe55c7758093af945e88036d88

hpi:run was broken under some conditions by ae5fdf6 due to remoteRepos being null in nested invocation.

src/main/java/org/jenkinsci/maven/plugins/hpi/MavenArtifact.java
@@ -42,6 +42,7 @@ public class MavenArtifact {
         this.artifactFactory = artifactFactory;
         this.builder = builder;
         this.remoteRepositories = remoteRepositories;
+        remoteRepositories.size(); // null check
         this.localRepository = localRepository;
     }
 

src/main/java/org/jenkinsci/maven/plugins/hpi/RunMojo.java
@@ -127,6 +127,9 @@ public class RunMojo extends AbstractJetty6Mojo {
     @Component
     protected ArtifactFactory artifactFactory;
 
+    @Parameter(defaultValue = "${project.remoteArtifactRepositories}", readonly = true, required = true)
+    protected List<ArtifactRepository> remoteRepos;
+
     @Parameter(defaultValue = "${localRepository}")
     protected ArtifactRepository localRepository;
 
@@ -385,6 +388,7 @@ public class RunMojo extends AbstractJetty6Mojo {
      * <p>
      * All I want to do here is to invoke the hpl target.
      * there must be a better way to do this!
+     * (jglick: perhaps https://github.com/TimMoore/mojo-executor would work?)
      *
      * <p>
      * Besides, if the user wants to change the plugin name, etc,
@@ -403,6 +407,12 @@ public class RunMojo extends AbstractJetty6Mojo {
         hpl.jenkinsCoreId = this.jenkinsCoreId;
         hpl.pluginFirstClassLoader = this.pluginFirstClassLoader;
         hpl.maskClasses = this.maskClasses;
+        hpl.remoteRepos = this.remoteRepos;
+        /* As needed:
+        hpl.artifactFactory = this.artifactFactory;
+        hpl.artifactResolver = this.artifactResolver;
+        hpl.artifactMetadataSource = this.artifactMetadataSource;
+        */
         hpl.execute();
     }
 