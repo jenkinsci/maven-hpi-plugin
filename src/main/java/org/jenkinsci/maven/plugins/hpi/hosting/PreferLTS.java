a86cf885d1fcd0593715addd798bf0ac582f9744
Baptiste Mathus <batmat@batmat.net>
2016-04-21 21:49:28 +0200

    Support 2.x plugin parent pom

--- src/main/java/org/jenkinsci/maven/plugins/hpi/hosting/PreferLTS.java
+++ src/main/java/org/jenkinsci/maven/plugins/hpi/hosting/PreferLTS.java
@@ -6,23 +6,30 @@ import org.jenkinsci.maven.plugins.hpi.hosting.VerificationMessage.Severity;
 public class PreferLTS implements VerificationRule {
 	@Override
 	public VerificationMessage validate(MavenProject project) {
-		MavenProject jenkinsParent = findJenkinsParentPom(project);
+		MavenProject jenkinsParent = findPluginParentPom(project);
 		if (jenkinsParent == null) {
 			return new VerificationMessage(
 					"Your plugin doesn't seem to have the Jenkins parent pom in its POM hierarchy. "
 							+ "Jenkins plugins generally inherit from the Jenkins parent POM.",
 					Severity.WARNING);
 		}
-		System.out.println(jenkinsParent);
-		if (jenkinsParent.getVersion().split("\\.").length != 3) {
+		String jenkinsVersion = jenkinsParent.getVersion();
+
+		// In 2.x plugin-pom became a standalone project https://github.com/jenkinsci/plugin-pom
+		// In that case, the Jenkins version is specified in the property "jenkins.version".
+		if(jenkinsVersion.startsWith("2."))
+		{
+			jenkinsVersion = (String) project.getProperties().get("jenkins.version");
+		}
+		if (jenkinsVersion.split("\\.").length != 3) {
 			return new VerificationMessage("Your plugin does not seem to have a LTS Jenkins release. In general, "
-					+ "it's preferrable to use an LTS version as parent version.",
+					+ "it's preferable to use an LTS version as parent version.",
 					Severity.INFO);
 		}
 		return null;
 	}
 
-	private MavenProject findJenkinsParentPom(MavenProject project) {
+	private MavenProject findPluginParentPom(MavenProject project) {
 		if ("org.jenkins-ci.plugins".equals(project.getGroupId()) && "plugin".equals(project.getArtifactId())) {
 			return project;
 		}
@@ -30,6 +37,6 @@ public class PreferLTS implements VerificationRule {
 		if (parent == null) {
 			return null;
 		}
-		return findJenkinsParentPom(parent);
+		return findPluginParentPom(parent);
 	}
 }
