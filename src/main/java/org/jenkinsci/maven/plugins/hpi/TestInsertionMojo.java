d6f1396d5c90e091a3721c592a4a4f57ab3c729a
Jesse Glick <jglick@cloudbees.com>
2017-06-23 12:31:36 -0400

    Fixing unchecked/rawtypes warnings in InjectedTest.

--- src/main/java/org/jenkinsci/maven/plugins/hpi/TestInsertionMojo.java
+++ src/main/java/org/jenkinsci/maven/plugins/hpi/TestInsertionMojo.java
@@ -117,7 +117,7 @@ public class TestInsertionMojo extends AbstractJenkinsMojo {
             w.println("public class " + injectedTestName + " extends junit.framework.TestCase {");
             w.println("  public static junit.framework.Test suite() throws Exception {");
             w.println("    System.out.println(\"Running tests for \"+" + quote(project.getGroupId() + ":" + project.getArtifactId() + ":" + project.getVersion())+");");
-            w.println("    Map parameters = new HashMap();");
+            w.println("    Map<String, Object> parameters = new HashMap<String, Object>();");
             w.println("    parameters.put(\"basedir\","+quote(project.getBasedir().getAbsolutePath())+");");
             w.println("    parameters.put(\"artifactId\","+quote(project.getArtifactId())+");");
             w.println("    parameters.put(\"packaging\","+quote(project.getPackaging())+");");
