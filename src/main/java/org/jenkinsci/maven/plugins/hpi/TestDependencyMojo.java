8b974bfdd33b7d7031fdd4450198e00139a0c872
Jesse Glick <jglick@cloudbees.com>
2017-08-04 17:49:48 -0400

    Partial implementation of useUpperBounds.

--- src/main/java/org/jenkinsci/maven/plugins/hpi/TestDependencyMojo.java
+++ src/main/java/org/jenkinsci/maven/plugins/hpi/TestDependencyMojo.java
@@ -9,17 +9,31 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.commons.lang.StringUtils;
 import org.apache.maven.artifact.DefaultArtifact;
 import org.apache.maven.artifact.resolver.AbstractArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactCollector;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.apache.maven.artifact.versioning.OverConstrainedVersionException;
 import org.apache.maven.artifact.versioning.VersionRange;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.dependency.tree.DependencyNode;
+import org.apache.maven.shared.dependency.tree.DependencyTreeBuilder;
+import org.apache.maven.shared.dependency.tree.DependencyTreeBuilderException;
+import org.apache.maven.shared.dependency.tree.traversal.DependencyNodeVisitor;
 
 /**
  * Places test-dependency plugins into somewhere the test harness can pick up.
@@ -32,6 +46,12 @@ import org.apache.maven.plugins.annotations.Parameter;
  */
 @Mojo(name="resolve-test-dependencies", requiresDependencyResolution = ResolutionScope.TEST)
 public class TestDependencyMojo extends AbstractHpiMojo {
+
+    @Component
+    private DependencyTreeBuilder dependencyTreeBuilder;
+
+    @Component
+    private ArtifactCollector collector;
 
     /**
      * List of dependency version overrides in the form {@code groupId:artifactId:version} to apply during testing.
@@ -97,7 +117,56 @@ public class TestDependencyMojo extends AbstractHpiMojo {
 
         if (overrideVersions != null) {
             if (useUpperBounds) {
-                throw new MojoExecutionException("TODO useUpperBounds not yet supported");
+                DependencyNode node;
+                try {
+                    MavenProject shadow = (MavenProject) project.clone();
+                    // first pass: adjust direct dependencies in place
+                    Set<String> updated = new HashSet<>();
+                    @SuppressWarnings("unchecked")
+                    Set<Artifact> dependencyArtifacts = shadow.getDependencyArtifacts(); // mutable; seems to be what DefaultDependencyTreeBuilder cares about
+                    for (Artifact art : dependencyArtifacts) {
+                        String key = art.getGroupId() + ":" + art.getArtifactId();
+                        String overrideVersion = overrides.get(key);
+                        if (overrideVersion != null) {
+                            getLog().debug("For dependency analysis, updating " + key + " from " + art.getVersion() + " to " + overrideVersion);
+                            art.setVersion(overrideVersion);
+                            updated.add(key);
+                        }
+                    }
+                    // second pass: add direct dependencies for transitive dependencies that need to be bumped
+                    @SuppressWarnings("unchecked")
+                    Set<Artifact> artifacts = shadow.getArtifacts();
+                    Set<String> transitiveUpdated = new HashSet<>();
+                    for (Artifact art : artifacts) {
+                        String key = art.getGroupId() + ":" + art.getArtifactId();
+                        if (updated.contains(key)) {
+                            continue; // already handled above
+                        }
+                        String overrideVersion = overrides.get(key);
+                        if (overrideVersion != null) {
+                            getLog().info("For dependency analysis, updating transitive " + key + " from " + art.getVersion() + " to " + overrideVersion);
+                            dependencyArtifacts.add(replace(art, overrideVersion));
+                            transitiveUpdated.add(key);
+                        }
+                    }
+                    Set<String> unapplied = new HashSet<>(overrides.keySet());
+                    unapplied.removeAll(updated);
+                    unapplied.removeAll(transitiveUpdated);
+                    if (!unapplied.isEmpty()) {
+                        throw new MojoFailureException("could not find dependencies " + unapplied);
+                    }
+                    getLog().debug("adjusted dependencyArtifacts: " + dependencyArtifacts);
+                    node = dependencyTreeBuilder.buildDependencyTree(shadow, localRepository, artifactFactory, artifactMetadataSource, /* all scopes */null, collector);
+                } catch (DependencyTreeBuilderException | CloneNotSupportedException x) {
+                    throw new MojoExecutionException("could not analyze dependency tree for useUpperBounds: " + x, x);
+                }
+                RequireUpperBoundDepsVisitor visitor = new RequireUpperBoundDepsVisitor();
+                node.accept(visitor);
+                Map<String, String> upperBounds = visitor.upperBounds();
+                if (!upperBounds.isEmpty()) {
+                    getLog().debug("Applying upper bounds: " + upperBounds);
+                    overrides.putAll(upperBounds);
+                }
             }
             List<String> additionalClasspathElements = new ArrayList<>();
             List<String> classpathDependencyExcludes = new ArrayList<>();
@@ -140,6 +209,122 @@ public class TestDependencyMojo extends AbstractHpiMojo {
             throw new MojoExecutionException("could not find " + a + " in version " + version + ": " + x, x);
         }
         return a2;
+    }
+
+    // Adapted from RequireUpperBoundDeps @ 3.0.0-M1. TODO delete extraneous stuff and simplify to the logic we actually need here:
+    private class RequireUpperBoundDepsVisitor implements DependencyNodeVisitor {
+
+        private boolean uniqueVersions;
+
+        public void setUniqueVersions(boolean uniqueVersions) {
+            this.uniqueVersions = uniqueVersions;
+        }
+
+        private Map<String, List<DependencyNodeHopCountPair>> keyToPairsMap
+                = new LinkedHashMap<String, List<DependencyNodeHopCountPair>>();
+
+        public boolean visit(DependencyNode node) {
+            DependencyNodeHopCountPair pair = new DependencyNodeHopCountPair(node);
+            String key = pair.constructKey();
+            List<DependencyNodeHopCountPair> pairs = keyToPairsMap.get(key);
+            if (pairs == null) {
+                pairs = new ArrayList<DependencyNodeHopCountPair>();
+                keyToPairsMap.put(key, pairs);
+            }
+            pairs.add(pair);
+            Collections.sort(pairs);
+            return true;
+        }
+
+        public boolean endVisit(DependencyNode node) {
+            return true;
+        }
+
+        // added for TestDependencyMojo in place of getConflicts/containsConflicts
+        @SuppressWarnings("unchecked")
+        public Map<String, String> upperBounds() {
+            Map<String, String> r = new HashMap<>();
+            // TODO this does not suffice; does not find that workflow-api needs to go from 2.11 to 2.16, presumably because it was not a direct dependency to begin with
+            for (List<DependencyNodeHopCountPair> pairs : keyToPairsMap.values()) {
+                DependencyNodeHopCountPair resolvedPair = pairs.get(0);
+
+                // search for artifact with lowest hopCount
+                for (DependencyNodeHopCountPair hopPair : pairs.subList(1, pairs.size())) {
+                    if (hopPair.getHopCount() < resolvedPair.getHopCount()) {
+                        resolvedPair = hopPair;
+                    }
+                }
+
+                ArtifactVersion resolvedVersion = resolvedPair.extractArtifactVersion(uniqueVersions, false);
+
+                for (DependencyNodeHopCountPair pair : pairs) {
+                    ArtifactVersion version = pair.extractArtifactVersion(uniqueVersions, true);
+                    if (resolvedVersion.compareTo(version) < 0) {
+                        Artifact artifact = resolvedPair.node.getArtifact();
+                        String key = artifact.getGroupId() + ":" + artifact.getArtifactId();
+                        getLog().info("for " + key + ", upper bounds forces an upgrade from " + resolvedVersion + " to " + version);
+                        r.put(key, version.toString());
+                    }
+                }
+            }
+            return r;
+        }
+
+    }
+
+    private static class DependencyNodeHopCountPair implements Comparable<DependencyNodeHopCountPair> {
+
+        private DependencyNode node;
+
+        private int hopCount;
+
+        private DependencyNodeHopCountPair(DependencyNode node) {
+            this.node = node;
+            countHops();
+        }
+
+        private void countHops() {
+            hopCount = 0;
+            DependencyNode parent = node.getParent();
+            while (parent != null) {
+                hopCount++;
+                parent = parent.getParent();
+            }
+        }
+
+        private String constructKey() {
+            Artifact artifact = node.getArtifact();
+            return artifact.getGroupId() + ":" + artifact.getArtifactId();
+        }
+
+        public DependencyNode getNode() {
+            return node;
+        }
+
+        private ArtifactVersion extractArtifactVersion(boolean uniqueVersions, boolean usePremanagedVersion) {
+            if (usePremanagedVersion && node.getPremanagedVersion() != null) {
+                return new DefaultArtifactVersion(node.getPremanagedVersion());
+            }
+
+            Artifact artifact = node.getArtifact();
+            String version = uniqueVersions ? artifact.getVersion() : artifact.getBaseVersion();
+            if (version != null) {
+                return new DefaultArtifactVersion(version);
+            }
+            try {
+                return artifact.getSelectedVersion();
+            } catch (OverConstrainedVersionException e) {
+                throw new RuntimeException("Version ranges problem with " + node.getArtifact(), e);
+            }
+        }
+
+        public int getHopCount() {
+            return hopCount;
+        }
+
+        public int compareTo(DependencyNodeHopCountPair other) {
+            return Integer.valueOf(hopCount).compareTo(Integer.valueOf(other.getHopCount()));
+        }
     }
 
 }
