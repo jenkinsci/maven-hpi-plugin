5e7d7214e652994ceb5965f28c2c4aeea538766e
Vincent Latombe <vincent@latombe.net>
2019-09-10 15:34:24 +0200

    Fix reviews

--- src/it/JENKINS-58771-packaged-plugins/verify.groovy
+++ src/it/JENKINS-58771-packaged-plugins/verify.groovy
@@ -1,4 +1,4 @@
-assert ! new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib/joda-time-2.9.5.jar').exists()
-assert ! new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib/multiline-secrets-ui-1.0.jar').exists()
+assert new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib').listFiles().length == 1
+assert new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib/JENKINS-58771-packaged-plugins.jar').exists()
 
 return true
