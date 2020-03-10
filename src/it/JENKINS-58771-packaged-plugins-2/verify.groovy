1905afed45171abd6593414b07488c79b1f84f00
Vincent Latombe <vincent@latombe.net>
2020-03-10 09:42:37 +0100

    Add negative assertions

--- src/it/JENKINS-58771-packaged-plugins-2/verify.groovy
+++ src/it/JENKINS-58771-packaged-plugins-2/verify.groovy
@@ -1,3 +1,4 @@
+assert !new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib/jboss-marshalling-river-2.0.6.Final.jar').exists()
 assert new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib').listFiles().length == 1
 assert new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib/JENKINS-58771-packaged-plugins.jar').exists()
 
--- src/it/JENKINS-58771-packaged-plugins-3/verify.groovy
+++ src/it/JENKINS-58771-packaged-plugins-3/verify.groovy
@@ -1,3 +1,4 @@
+assert !new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib/jsr305-3.0.2.jar').exists()
 assert new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib').listFiles().length == 1
 assert new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib/JENKINS-58771-packaged-plugins.jar').exists()
 
