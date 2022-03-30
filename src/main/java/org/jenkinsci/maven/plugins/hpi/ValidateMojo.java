5943af8b6dfae2b916ffd6403bd24e317320a2be
Basil Crow <me@basilcrow.com>
2022-03-30 10:09:23 -0700

    Print warning if using deprecated `java.level` with new enough parent POM (#320)

--- src/main/java/org/jenkinsci/maven/plugins/hpi/ValidateMojo.java
+++ src/main/java/org/jenkinsci/maven/plugins/hpi/ValidateMojo.java
@@ -5,6 +5,7 @@ import io.jenkins.lib.versionnumber.JavaSpecificationVersion;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.project.MavenProject;
 
 /**
  * Make sure that we are running in the right environment.
@@ -23,6 +24,17 @@ public class ValidateMojo extends AbstractJenkinsMojo {
 
         if (new VersionNumber(findJenkinsVersion()).compareTo(new VersionNumber("2.204")) < 0) {
             throw new MojoExecutionException("This version of maven-hpi-plugin requires Jenkins 2.204 or later");
+        }
+
+        MavenProject parent = project.getParent();
+        if (parent != null
+                && parent.getGroupId().equals("org.jenkins-ci.plugins")
+                && parent.getArtifactId().equals("plugin")
+                && !parent.getProperties().containsKey("java.level")
+                && project.getProperties().containsKey("java.level")) {
+            getLog().warn("Ignoring deprecated java.level property."
+                    + " This property should be removed from your plugin's POM."
+                    + " In the future this warning will be changed to an error and will break the build.");
         }
     }
 }
