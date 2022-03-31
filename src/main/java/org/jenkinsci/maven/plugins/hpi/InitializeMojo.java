deea11a77fd7fc6de03667499e9b616f59b7b6f1
Basil Crow <me@basilcrow.com>
2022-03-31 10:01:42 -0700

    Dynamically configure Java compiler based on core (#323)

--- src/main/java/org/jenkinsci/maven/plugins/hpi/InitializeMojo.java
+++ src/main/java/org/jenkinsci/maven/plugins/hpi/InitializeMojo.java
@@ -2,6 +2,7 @@ package org.jenkinsci.maven.plugins.hpi;
 
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.util.VersionNumber;
 import io.jenkins.lib.versionnumber.JavaSpecificationVersion;
 import java.io.File;
 import java.io.IOException;
@@ -26,7 +27,62 @@ public class InitializeMojo extends AbstractJenkinsMojo {
 
     @Override
     public void execute() throws MojoExecutionException {
+        setCompilerProperties();
         setSurefireProperties();
+    }
+
+    private void setCompilerProperties() throws MojoExecutionException {
+        if (!project.getProperties().containsKey("maven.compiler.source")
+                && !project.getProperties().containsKey("maven.compiler.release")) {
+            // On an older plugin parent POM that predates the setting of these values as Maven properties.
+            return;
+        }
+
+        JavaSpecificationVersion javaVersion = getMinimumJavaVersion();
+        if (JavaSpecificationVersion.forCurrentJVM().isOlderThan(new VersionNumber("9"))) {
+            // Should always be set already, but just in case...
+            setProperty("maven.compiler.source", javaVersion.toString());
+            setProperty("maven.compiler.target", javaVersion.toString());
+            setProperty("maven.compiler.testSource", javaVersion.toString());
+            setProperty("maven.compiler.testTarget", javaVersion.toString());
+            // Should never be set already, but just in case...
+            unsetProperty("maven.compiler.release");
+            unsetProperty("maven.compiler.testRelease");
+        } else {
+            /*
+             * When compiling with a Java 9+ compiler, we always rely on "release" in favor of "source" and "target",
+             * even when compiling to Java 8 bytecode.
+             */
+            setProperty("maven.compiler.release", Integer.toString(javaVersion.toReleaseVersion()));
+            setProperty("maven.compiler.testRelease", Integer.toString(javaVersion.toReleaseVersion()));
+
+            // "release" serves the same purpose as Animal Sniffer.
+            setProperty("animal.sniffer.skip", "true");
+
+            /*
+             * While it does not hurt to have these set to the Java specification version, it is also not needed when
+             * "release" is in use.
+             */
+            unsetProperty("maven.compiler.source");
+            unsetProperty("maven.compiler.target");
+            unsetProperty("maven.compiler.testSource");
+            unsetProperty("maven.compiler.testTarget");
+        }
+    }
+
+    private void setProperty(String key, String value) {
+        String currentValue = project.getProperties().getProperty(key);
+        if (currentValue == null || !currentValue.equals(value)) {
+            getLog().info("Setting " + key + " to " + value);
+            project.getProperties().setProperty(key, value);
+        }
+    }
+
+    private void unsetProperty(String key) {
+        if (project.getProperties().containsKey(key)) {
+            getLog().info("Unsetting " + key);
+            project.getProperties().remove(key);
+        }
     }
 
     private void setSurefireProperties() throws MojoExecutionException {
