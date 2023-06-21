cdac8959b47e06952614822a7efa9e14592378f8
Basil Crow <me@basilcrow.com>
2023-06-21 11:27:36 -0700

    Cleanup

--- src/main/java/org/jenkinsci/maven/plugins/hpi/extensions/AbstractLifecycleMappingProvider.java
+++ src/main/java/org/jenkinsci/maven/plugins/hpi/extensions/AbstractLifecycleMappingProvider.java
@@ -21,30 +21,40 @@ public abstract class AbstractLifecycleMappingProvider implements Provider<Lifec
         defaultLifecycle.setId(DEFAULT_LIFECYCLE_KEY);
         defaultLifecycle.setLifecyclePhases(bindings);
 
-        lifecycleMapping = new LifecycleMapping() {
-            @Override
-            public Map<String, Lifecycle> getLifecycles() {
-                return Map.of(DEFAULT_LIFECYCLE_KEY, defaultLifecycle);
-            }
-
-            @Override
-            public List<String> getOptionalMojos(String lifecycle) {
-                return null;
-            }
-
-            @Override
-            public Map<String, String> getPhases(String lifecycle) {
-                if (DEFAULT_LIFECYCLE_KEY.equals(lifecycle)) {
-                    return defaultLifecycle.getPhases();
-                } else {
-                    return null;
-                }
-            }
-        };
+        lifecycleMapping = new DefaultLifecycleMapping(defaultLifecycle);
     }
 
     @Override
     public LifecycleMapping get() {
         return lifecycleMapping;
+    }
+
+    private static class DefaultLifecycleMapping implements LifecycleMapping {
+        private final Lifecycle lifecycle;
+
+        DefaultLifecycleMapping(Lifecycle lifecycle) {
+            this.lifecycle = lifecycle;
+        }
+
+        @Override
+        public Map<String, Lifecycle> getLifecycles() {
+            return Map.of(DEFAULT_LIFECYCLE_KEY, lifecycle);
+        }
+
+        @Deprecated
+        @Override
+        public List<String> getOptionalMojos(String lifecycle) {
+            return null;
+        }
+
+        @Deprecated
+        @Override
+        public Map<String, String> getPhases(String lifecycle) {
+            if (DEFAULT_LIFECYCLE_KEY.equals(lifecycle)) {
+                return this.lifecycle.getPhases();
+            } else {
+                return null;
+            }
+        }
     }
 }
