799b9b2b0b05bf5fe288c3cf9c612b7d9da7326e
Akash Manna <akash.manna.mymail@gmail.com>
2025-09-10 00:59:25 +0530

    modified

--- src/it/JENKINS-58771-packaged-plugins-3/verify.groovy
+++ src/it/JENKINS-58771-packaged-plugins-3/verify.groovy
@@ -1,5 +1,7 @@
 assert !new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib/jsr305-3.0.2.jar').exists()
-assert new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib').listFiles().length == 1
+def libDir = new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib')
+def libFiles = libDir.listFiles()
+assert libFiles != null && libFiles.length == 1
 assert new File(basedir, 'target/JENKINS-58771-packaged-plugins/WEB-INF/lib/JENKINS-58771-packaged-plugins.jar').exists()
 
 return true
